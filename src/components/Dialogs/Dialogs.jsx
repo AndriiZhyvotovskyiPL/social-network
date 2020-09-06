4ebe62ccf1937c6ab4b0d6e310d1006a93c174f9
AndriiZhyvotovskyiPL <andriizhyvotovskyi@gmail.com>
2020-09-06 21:41:15 +0200

    Layout and styling of the message page. Use NavLink.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,19 +7,19 @@ const Dialogs = (props) => {
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog}>
-                    Dimych
-                </div>
-                <div className={`${s.dialog} ${s.active}`}>
-                    Bro
+                    <NavLink to="/dialogs/1" activeClassName={s.activeLink}>Dimych</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Natali
+                    <NavLink to="/dialogs/2" activeClassName={s.activeLink}>Bro</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Martin
+                    <NavLink to="/dialogs/3" activeClassName={s.activeLink}>Natali</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Alex
+                    <NavLink to="/dialogs/4" activeClassName={s.activeLink}>Martin</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/5" activeClassName={s.activeLink}>Alex</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
