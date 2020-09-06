a0a846dcbd81d976da5ff6fd432311afef0d6d4b
AndriiZhyvotovskyiPL <andriizhyvotovskyi@gmail.com>
2020-09-06 20:39:09 +0200

    Layout and styling of the message page.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,38 @@
 import React from "react";
 import s from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Dimych
+                </div>
+                <div className={`${s.dialog} ${s.active}`}>
+                    Bro
+                </div>
+                <div className={s.dialog}>
+                    Natali
+                </div>
+                <div className={s.dialog}>
+                    Martin
+                </div>
+                <div className={s.dialog}>
+                    Alex
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi, how are you?
+                </div>
+                <div className={s.message}>
+                    I don't no
+                </div>
+                <div className={s.message}>
+                    Ok bro
+                </div>
+            </div>
         </div>
     );
 }
