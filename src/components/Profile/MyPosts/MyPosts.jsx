f2bd3bcce8aac84a4a305a213da7cf599dc382e9
AndriiZhyvotovskyiPL <andriizhyvotovskyi@gmail.com>
2020-09-18 11:38:16 +0200

    Refactoring Dialogs.jsx and MyPosts.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,10 +3,12 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
-    let postData = [
+    let posts = [
         {id : 1, message : 'Hi, how are you?', likesCount : 15},
         {id : 2, message : 'It\'s my first post', likesCount : 23}
     ]
+
+    let postsElements = posts.map( p => <Post message={p.message} likesCount={p.likesCount}/>)
     return (
         <div className={s.postsBlock}>
             <h3>My post</h3>
@@ -19,8 +21,7 @@ const MyPosts = (props) => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
-                <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
+                {postsElements}
             </div>
         </div>
     );
