fbe7e741b5053d8333f1555a90b933d1eb2bb1f1
AndriiZhyvotovskyiPL <andriizhyvotovskyi@gmail.com>
2020-09-30 21:27:16 +0200

    Refactoring index.js use the state.js file to store data.

--- src/index.js
+++ src/index.js
@@ -3,30 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-let posts = [
-    {id : 1, message : 'Hi, how are you?', likesCount : 15},
-    {id : 2, message : 'It\'s my first post', likesCount : 23}
-]
-
-let dialogs = [
-    {id : 1, name : 'Dimych'},
-    {id : 2, name : 'Bro'},
-    {id : 3, name : 'Natali'},
-    {id : 4, name : 'Martin'},
-    {id : 5, name : 'Alex'}
-]
-
-let messages = [
-    {id : 1, message : 'Hi, how are you?'},
-    {id : 2, message : 'I don\'t know'},
-    {id : 3, message : 'Ok bro'},
-    {id : 4, message : 'U2'}
-]
+import state from './redux/state';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages}/>
+    <App state={state}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/state.js
@@ -0,0 +1,25 @@
+let state = {
+    profilePage:{
+        posts: [
+            {id : 1, message : 'Hi, how are you?', likesCount : 15},
+            {id : 2, message : 'It\'s my first post', likesCount : 23}
+        ]
+    },
+    dialogsPage:{
+        dialogs: [
+            {id : 1, name : 'Dimych'},
+            {id : 2, name : 'Bro'},
+            {id : 3, name : 'Natali'},
+            {id : 4, name : 'Martin'},
+            {id : 5, name : 'Alex'}
+        ],
+        messages: [
+            {id : 1, message : 'Hi, how are you?'},
+            {id : 2, message : 'I don\'t know'},
+            {id : 3, message : 'Ok bro'},
+            {id : 4, message : 'U2'}
+        ]
+    }
+}
+
+export default state;
