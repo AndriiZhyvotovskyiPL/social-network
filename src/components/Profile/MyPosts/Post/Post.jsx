f102c65238fd648a53b9cca8737ad162ea88140a
AndriiZhyvotovskyiPL <andriizhyvotovskyi@gmail.com>
2020-10-01 20:36:43 +0200

    Refactoring

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -4,9 +4,7 @@ import s from "./Posts.module.css";
 const Post = (props) => {
     return (
         <div className={s.item}>
-            <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRRL1brS7VjomKn2iqD-LXIyJbiClhK5Ci47A&usqp=CAU"
-                alt=""/>
+            <img src={props.src} alt=""/>
             {props.message}
             <div>
                 <span>like {props.likesCount}</span>
