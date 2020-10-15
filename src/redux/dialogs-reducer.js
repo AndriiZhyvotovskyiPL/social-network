c1b8f416e2fc1556806fc218595a040c3c408691
AndriiZhyvotovskyiPL <andriizhyvotovskyi@gmail.com>
2020-10-15 21:53:59 +0200

    Refactoring

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -2,22 +2,28 @@ const SEND_MESSAGE = 'SEND-MESSAGE';
 const UPDATE_MESSAGE = 'UPDATE-MESSAGE';
 
 const dialogsReducer = (state, action) => {
-    if (action.type === SEND_MESSAGE) {
-        let newMessage = {
-            id: 5,
-            message: state.newMessageText
-        }
-        state.messages.outgoing.push(newMessage);
-        state.newMessageText = '';
-    } else if (action.type === UPDATE_MESSAGE) {
-        state.newMessageText = action.newMessageText;
+    switch (action.type) {
+        case SEND_MESSAGE:
+            let newMessage = {
+                id: 5,
+                message: state.newMessageText
+            }
+            state.messages.outgoing.push(newMessage);
+            state.newMessageText = '';
+            return state;
+        case UPDATE_MESSAGE:
+            state.newMessageText = action.newMessageText;
+            return state;
+        default:
+            return state;
     }
-    return state;
 }
+
 export const sendMessageActionCreate = () => ({type: SEND_MESSAGE})
 
 export const updateMessageActionCreator = (text) => ({
     type: UPDATE_MESSAGE,
     newMessageText: text
 })
+
 export default dialogsReducer;
