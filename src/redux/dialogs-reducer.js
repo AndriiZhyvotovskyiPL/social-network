f060522d4f6b7a0d359cace164e1716162adc952
AndriiZhyvotovskyiPL <andriizhyvotovskyi@gmail.com>
2020-10-16 22:05:56 +0200

    Create initialState and use reducer in redux-store.js

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,31 @@
 const SEND_MESSAGE = 'SEND-MESSAGE';
 const UPDATE_MESSAGE = 'UPDATE-MESSAGE';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    dialogs: [
+        {id: 1, name: 'Dimych', src: 'https://i.wpimg.pl/1777x0/d.wpimg.pl/1294094975--769611975/avatar.jpg'},
+        {id: 2, name: 'Bro', src: 'https://i.wpimg.pl/1777x0/d.wpimg.pl/1294094975--769611975/avatar.jpg'},
+        {id: 3, name: 'Natali', src: 'https://i.wpimg.pl/1777x0/d.wpimg.pl/1294094975--769611975/avatar.jpg'},
+        {id: 4, name: 'Martin', src: 'https://i.wpimg.pl/1777x0/d.wpimg.pl/1294094975--769611975/avatar.jpg'},
+        {id: 5, name: 'Alex', src: 'https://i.wpimg.pl/1777x0/d.wpimg.pl/1294094975--769611975/avatar.jpg'}
+    ],
+    messages: {
+        income: [
+            {id: 1, message: 'Hi, how are you?'},
+            {id: 2, message: 'I don\'t know'},
+            {id: 3, message: 'Ok bro'},
+            {id: 4, message: 'U2'}
+        ],
+        outgoing: [
+            {id: 1, message: 'Yep'},
+            {id: 2, message: 'Last night'},
+            {id: 3, message: 'Ok bro'},
+            {id: 4, message: '5 minute'}
+        ]
+    },
+    newMessageText: 'Message text'
+}
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case SEND_MESSAGE:
             let newMessage = {
