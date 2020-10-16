fb55103cefde6fb28e05b7863437ca0ee3e82bf7
AndriiZhyvotovskyiPL <andriizhyvotovskyi@gmail.com>
2020-10-16 22:05:48 +0200

    Create initialState and use reducer in redux-store.js

--- src/redux/sidebar-reducer.js
+++ src/redux/sidebar-reducer.js
@@ -1,4 +1,12 @@
-const sidebarReducer = (state, action) => {
+let initialState = {
+    friends: [
+        {id: 1, name: 'Bro', src: 'https://i.wpimg.pl/1777x0/d.wpimg.pl/1294094975--769611975/avatar.jpg'},
+        {id: 1, name: 'Dimych', src: 'https://i.wpimg.pl/1777x0/d.wpimg.pl/1294094975--769611975/avatar.jpg'},
+        {id: 1, name: 'Natali', src: 'https://i.wpimg.pl/1777x0/d.wpimg.pl/1294094975--769611975/avatar.jpg'}
+    ]
+}
+
+const sidebarReducer = (state = initialState, action) => {
     return state;
 }
 
