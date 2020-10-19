0926428de92ffa70838e31a1466f3d80c7044690
AndriiZhyvotovskyiPL <andriizhyvotovskyi@gmail.com>
2020-10-19 21:35:14 +0200

    Moving the logic out to the container component MypPostsContainer.jsx Refactoring

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
-import {addPostActionCreator, updatePostTextActionCreator} from "../../../redux/profile-reducer";
 
 const MyPosts = (props) => {
-    let postsElements = props.state.posts.map(p => <Post message={p.message}
+    let postsElements = props.posts.map(p => <Post message={p.message}
                                                          likesCount={p.likesCount}
                                                          src={p.src}/>)
 
     let newPostElement = React.createRef();
 
-    let addPost = () => {
-        props.dispatch(addPostActionCreator());
+    let onAddPost = () => {
+        props.addPost();
     }
 
     let onChangePost = () => {
         let text = newPostElement.current.value;
-        props.dispatch(updatePostTextActionCreator(text));
+        props.updatePostText(text);
     }
 
     return (
@@ -26,10 +25,10 @@ const MyPosts = (props) => {
                 <div>
                     <textarea onChange={onChangePost}
                               ref={newPostElement}
-                              value={props.state.newPostText}/>
+                              value={props.newPostText}/>
                 </div>
                 <div>
-                    <button onClick={addPost}>Add post</button>
+                    <button onClick={onAddPost}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
