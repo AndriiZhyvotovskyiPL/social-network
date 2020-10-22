fc53d845137387f24965b8aa11fede2cbb0eeb63
AndriiZhyvotovskyiPL <andriizhyvotovskyi@gmail.com>
2020-10-23 00:03:53 +0200

    Use NavBarComponent

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
-import NavBar from "./components/NavBar/NavBar";
 import Profile from "./components/Profile/Profile";
 import {Route} from "react-router-dom";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Setting from "./components/Setting/Setting";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import StoreContext from "./StoreContext";
+import NavBarContainer from "./components/NavBar/NavBarContainer";
 
 const App = (props) => {
     return (
         <div className="app-wrapper">
             <Header/>
-            <StoreContext.Consumer>
-                {(store) => {
-                    let state = store.getState();
-                    return (
-                        <NavBar state={state.sidebar}/>
-                    )
-                }
-                }
-            </StoreContext.Consumer>
+            <NavBarContainer/>
             <div className="app-wrapper-content">
                 <Route path="/profile" render={() =>
                     <Profile store={props.store}/>
