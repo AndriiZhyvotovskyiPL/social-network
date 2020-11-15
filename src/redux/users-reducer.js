3ef949fa32dc6d20196745cad57519073a94d3b7
AndriiZhyvotovskyiPL <andriizhyvotovskyi@gmail.com>
2020-11-15 12:22:35 +0100

    Refactoring

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,31 +3,9 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS'
 
 let initialState = {
-    users: [
-        /*{
-            id: 1,
-            followed: false,
-            fullName: 'Dimych',
-            status: 'I am a boss',
-            location: {city: 'Minsk', country: 'Belarus'}
-        },
-        {
-            id: 1,
-            followed: true,
-            fullName: 'Andrew',
-            status: 'I am a boss too',
-            location: {city: 'Bielsko-Biala', country: 'Poland'}
-        },
-        {
-            id: 1,
-            followed: false,
-            fullName: 'Roman',
-            status: 'I am a boss too',
-            location: {city: 'Moscow', country: 'Russia'}
-        },*/
-    ]
+    users: [ ]
 }
-const profileReducer = (state = initialState, action) => {
+const usersReducer = (state = initialState, action) => {
     switch (action.type) {
         case FOLLOW:
             return {
@@ -63,6 +41,6 @@ export const followAC = (userId) => ({type: FOLLOW, userId})
 
 export const unfollowAC = (userId) => ({type: UNFOLLOW, userId})
 
-export const setUsersAC = (users) => ({type; SET_USERS, users})
+export const setUsersAC = (users) => ({type: SET_USERS, users})
 
-export default profileReducer;
+export default usersReducer;
