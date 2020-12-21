d745aeb0e7ab80459e963e1296eb809756066b8c
Andrii Zhyvotovskyi <70468344+AndriiZhyvotovskyiPL@users.noreply.github.com>
2020-12-21 22:10:46 +0100

    I use ProfileInfo instead of Profile.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
-import Profile from "./components/Profile/Profile";
 import {Route} from "react-router-dom";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
@@ -9,6 +8,7 @@ import Setting from "./components/Setting/Setting";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import NavBarContainer from "./components/NavBar/NavBarContainer";
 import UsersContainer from "./components/Users/UsersContainer";
+import ProfileContainer from "./components/Profile/ProfileInfo/ProfileContainer";
 
 const App = (props) => {
     return (
@@ -16,7 +16,7 @@ const App = (props) => {
             <Header/>
             <NavBarContainer/>
             <div className="app-wrapper-content">
-                <Route path="/profile" render={() => <Profile/>}/>
+                <Route path="/profile" render={() => <ProfileContainer/>}/>
                 <Route path="/dialogs" render={() => <DialogsContainer/>}/>
                 <Route path="/news" render={() => <News/>}/>
                 <Route path="/music" render={() => <Music/>}/>
