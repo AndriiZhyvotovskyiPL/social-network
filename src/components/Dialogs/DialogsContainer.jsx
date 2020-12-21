76ff03b953e08650c19f86d8eef7ac1cd115303c
Andrii Zhyvotovskyi <70468344+AndriiZhyvotovskyiPL@users.noreply.github.com>
2020-12-21 18:08:51 +0100

    Use the object with action creators in the function instead of mapDispatchToProps. Refactoring.

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {sendMessageActionCreate, updateMessageActionCreator} from "../../redux/dialogs-reducer";
+import {sendMessage, updateMessage} from "../../redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
 import {connect} from "react-redux";
 
@@ -12,17 +12,4 @@ let mapStateToProps = (state) => {
     }
 }
 
-let mapDispatchToProps = (dispatch) =>{
-    return {
-        sendMessage: () => {
-            dispatch(sendMessageActionCreate());
-        },
-        updateMessage: (text) => {
-            dispatch(updateMessageActionCreator(text));
-        }
-    }
-}
-
-const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs);
-
-export default DialogsContainer;
+export default connect(mapStateToProps, {sendMessage, updateMessage})(Dialogs);
