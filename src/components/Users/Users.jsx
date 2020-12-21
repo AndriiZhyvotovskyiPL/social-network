8fe18e3e075ef9e4b9ae210fb00ff24afd8feb51
Andrii Zhyvotovskyi <70468344+AndriiZhyvotovskyiPL@users.noreply.github.com>
2020-12-21 22:03:12 +0100

    Use NavLink to go to the profile by clicking on the photo.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from "./Users.module.css";
 import userPhoto from "../../assets/images/user.png";
+import {NavLink} from "react-router-dom";
 
 const Users = (props) => {
     let pageCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -25,7 +26,10 @@ const Users = (props) => {
                 props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.img} alt='image'/>
+                        <NavLink to={'/profile/' + u.id}>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.img}
+                                 alt='image'/>
+                        </NavLink>
                     </div>
                     <div>
                         {u.followed
