07660640258fa5843ffb0125a83fe43eb2598a22
Andrii Zhyvotovskyi <70468344+AndriiZhyvotovskyiPL@users.noreply.github.com>
2021-04-26 21:48:55 +0200

    Updates

---
+++ precache-manifest.3f06c13dc3f0c30ba793a9d2b52ca3f7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02cd82017804377beaee9554a53ba49a",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "161fb34425202afbdb28",
+    "url": "/social-network/static/css/3.f3e6c6ff.chunk.css"
+  },
+  {
+    "revision": "7921a51cd9fa114366d5",
+    "url": "/social-network/static/css/4.3b6725a5.chunk.css"
+  },
+  {
+    "revision": "bd57b757d70edf592a9e",
+    "url": "/social-network/static/css/5.b36df191.chunk.css"
+  },
+  {
+    "revision": "20683bfd89bccf235671",
+    "url": "/social-network/static/css/main.6bd2e001.chunk.css"
+  },
+  {
+    "revision": "4caabcaef52f922d4d53",
+    "url": "/social-network/static/js/2.cb08f237.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/2.cb08f237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "161fb34425202afbdb28",
+    "url": "/social-network/static/js/3.9186df4d.chunk.js"
+  },
+  {
+    "revision": "7921a51cd9fa114366d5",
+    "url": "/social-network/static/js/4.e15e9cc0.chunk.js"
+  },
+  {
+    "revision": "fe07165234709e61e0cdc05d4056de5c",
+    "url": "/social-network/static/js/4.e15e9cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd57b757d70edf592a9e",
+    "url": "/social-network/static/js/5.b2a50237.chunk.js"
+  },
+  {
+    "revision": "20683bfd89bccf235671",
+    "url": "/social-network/static/js/main.5e41de48.chunk.js"
+  },
+  {
+    "revision": "1188c69d39a570d05f8d",
+    "url": "/social-network/static/js/runtime-main.47363f3e.js"
+  },
+  {
+    "revision": "75ad70ec825607b9a05924436444578a",
+    "url": "/social-network/static/media/preloader.75ad70ec.svg"
+  },
+  {
+    "revision": "d6759143baffd6711527ab1dcffcf093",
+    "url": "/social-network/static/media/user.d6759143.png"
+  }
+]);
