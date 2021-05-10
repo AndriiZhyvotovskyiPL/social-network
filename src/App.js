55f294ad680b67bf53d0b78b81d1d052a6cafce0
Andrii Zhyvotovskyi <70468344+AndriiZhyvotovskyiPL@users.noreply.github.com>
2021-05-10 22:35:31 +0200

    I use a switch to optimize routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Route} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Setting from "./components/Setting/Setting";
@@ -12,16 +12,17 @@ import {initializedApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
 import {withSuspense} from "./hoc/withSuspense";
 
-const ProfileContainer = React.lazy(()=> import('./components/Profile/ProfileInfo/ProfileContainer'));
-const DialogsContainer = React.lazy(()=> import('./components/Dialogs/DialogsContainer'));
-const UsersContainer = React.lazy(()=> import('./components/Users/UsersContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileInfo/ProfileContainer'));
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
 
 class App extends React.Component {
     componentDidMount() {
         this.props.initializedApp();
     }
+
     render() {
-        if(!this.props.initialized) {
+        if (!this.props.initialized) {
             return <Preloader/>
         }
         return (
@@ -29,13 +30,17 @@ class App extends React.Component {
                 <HeaderContainer/>
                 <NavBarContainer/>
                 <div className="app-wrapper-content">
-                    <Route path="/profile/:userId?" render={withSuspense(ProfileContainer)}/>
-                    <Route path="/dialogs" render={withSuspense(DialogsContainer)}/>
-                    <Route path="/news" render={() => <News/>}/>
-                    <Route path="/music" render={() => <Music/>}/>
-                    <Route path="/users" render={withSuspense(UsersContainer)}/>
-                    <Route path="/setting" render={() => <Setting/>}/>
-                    <Route path="/login" render={() => <Login/>}/>
+                    <Switch>
+                        <Route exact path="/" render={() => <Redirect to='/profile'/>}/>
+                        <Route path="/profile/:userId?" render={withSuspense(ProfileContainer)}/>
+                        <Route path="/dialogs" render={withSuspense(DialogsContainer)}/>
+                        <Route path="/news" render={() => <News/>}/>
+                        <Route path="/music" render={() => <Music/>}/>
+                        <Route path="/users" render={withSuspense(UsersContainer)}/>
+                        <Route path="/setting" render={() => <Setting/>}/>
+                        <Route path="/login" render={() => <Login/>}/>
+                        <Route path="*" render={() => <div>404 NOT FOUND</div>}/>
+                    </Switch>
                 </div>
             </div>
         );
